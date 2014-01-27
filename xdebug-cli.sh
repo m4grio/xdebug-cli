0d2bd09c1b1b96443a48a1b810b5a5b8cf176699
m4grio <ahoy@m4grio.me>
2014-01-27 12:37:48 -0600

    Last changes

--- xdebug-cli.sh
+++ xdebug-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-php -d xdebug.remote_enable="1" -d xdebug.remote_port="9000" -d xdebug.remote_handler="dbgp" -d xdebug.remote_connect_back="1" -d xdebug.remote_autostart="1" -d xdebug.remote_host="`echo $SSH_CLIENT | cut -d "=" -f 2 | awk '{print $1}'`" $@
+php -d xdebug.cli_color=1 -d xdebug.remote_enable=1 -d xdebug.remote_port=9000 -d xdebug.remote_handler=dbgp -d xdebug.remote_connect_back=1 -d xdebug.remote_autostart=1 -d xdebug.remote_host=`echo $SSH_CLIENT | cut -d "=" -f 2 | awk '{print $1}'` $@
